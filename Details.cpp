c4e38259679e46c9126000d2a2c380a9ce22b522
SauravDasss <158020369+SauravDasss@users.noreply.github.com>
2024-01-29 16:47:38 +0530

    Add files via upload

--- Details.cpp
+++ Details.cpp
@@ -4,26 +4,26 @@ using namespace std;
 int main(){
 //string
 string name;
-cout<<"What is your name?";
+cout<<"What is your name? ";
 cin>>name;
 cout<<"My name is "<<name<<endl;
 
 //integer types
 int age;
-cout<<"What is your age?";
+cout<<"What is your age? ";
 cin >>age;
 cout<<"My age is "<<age<<endl;
 
 //integer value
 int weight;
-cout<<"What is your weight?";
+cout<<"What is your weight? ";
 cin >>weight;
 cout<<"My weight is "<<weight<<endl;
 
 
-//Character valeue
+//Character value
 char gender;
-cout<<"What is your gender?";
+cout<<"What is your gender? ";
 cin>>gender;
 cout<<"It is "<<gender<<endl;
 
