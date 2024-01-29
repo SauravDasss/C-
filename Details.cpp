a6bc99391b6206389a77fe37f765d44d50704416
SauravDasss <158020369+SauravDasss@users.noreply.github.com>
2024-01-29 15:39:32 +0530

    Add files via upload

---
+++ Details.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//string
+string name;
+cout<<"What is his name?";
+cin>>name;
+cout<<"His name is "<<name<<endl;
+
+//integer types
+int age;
+cout<<"What is his age?";
+cin >>age;
+cout<<"His age is "<<age<<endl;
+
+//integer value
+int weight;
+cout<<"What is his weight?";
+cin >>weight;
+cout<<"His weight is "<<weight<<endl;
+
+
+//Character valeue
+char gender;
+cout<<"What is your gender?";
+cin>>gender;
+cout<<"It is "<<gender<<endl;
+
+
+ return 0;
+}
